1ad9153a286707eb805d868990f0ba3e1d36433f
cedric.decoster <cedric.decoster@gmail.com>
2017-01-09 14:38:42 +0100

    added pirate warrior deck only missing finley. actualized HsProtoSim some picture for readme

---
+++ SimpleAi/Meta/Decks.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HsProtoSim.Model;
+
+namespace SimpleAi.Meta
+{
+    internal class Decks
+    {
+        public static List<Card> ShamanAggro => new List<Card>()
+        {
+            Cards.FromName("Mirror Image"),
+        };
+
+        public static List<Card> WarriorPirate => new List<Card>()
+        {
+            Cards.FromName("N'Zoth's First Mate"),
+            Cards.FromName("N'Zoth's First Mate"),
+            Cards.FromName("Upgrade!"),
+            Cards.FromName("Upgrade!"),
+            Cards.FromName("Fiery War Axe"),
+            Cards.FromName("Fiery War Axe"),
+            Cards.FromName("Heroic Strike"),
+            Cards.FromName("Heroic Strike"),
+            Cards.FromName("Bloodsail Cultist"),
+            Cards.FromName("Bloodsail Cultist"),
+            Cards.FromName("Frothing Berserker"),
+            Cards.FromName("Frothing Berserker"),
+            Cards.FromName("Kor'kron Elite"),
+            Cards.FromName("Kor'kron Elite"),
+            Cards.FromName("Arcanite Reaper"),
+            Cards.FromName("Arcanite Reaper"),
+            Cards.FromName("Patches the Pirate"),
+            // TODO Cards.FromName("Sir Finley Mrrgglton"),
+            Cards.FromName("Small-Time Buccaneer"),
+            Cards.FromName("Small-Time Buccaneer"),
+            Cards.FromName("Southsea Deckhand"),
+            Cards.FromName("Southsea Deckhand"),
+            Cards.FromName("Bloodsail Raider"),
+            Cards.FromName("Bloodsail Raider"),
+            Cards.FromName("Southsea Captain"),
+            Cards.FromName("Southsea Captain"),
+            Cards.FromName("Dread Corsair"),
+            Cards.FromName("Dread Corsair"),
+            Cards.FromName("Naga Corsair"),
+            Cards.FromName("Naga Corsair"),
+        };
+
+        public static List<Card> SimpleMage => new List<Card>()
+        {
+            Cards.FromName("Mirror Image"),
+            Cards.FromName("Sorcerer's Apprentice"),
+            Cards.FromName("Ice Lance"),
+            Cards.FromName("Frostbolt"),
+            Cards.FromName("Ironforge Rifleman"),
+            Cards.FromName("Mirror Image"),
+            Cards.FromName("Ironforge Rifleman"),
+            Cards.FromName("Gnomish Inventor"),
+            Cards.FromName("Fireball"),
+            Cards.FromName("Fireball"),
+            Cards.FromName("Gnomish Inventor"),
+            Cards.FromName("Ice Lance"),
+            Cards.FromName("Sorcerer's Apprentice"),
+            Cards.FromName("Frostwolf Grunt"),
+            Cards.FromName("Raid Leader"),
+            Cards.FromName("Kobold Geomancer"),
+            Cards.FromName("Stormpike Commando"),
+            Cards.FromName("Sen'jin Shieldmasta"),
+            Cards.FromName("Elven Archer"),
+            Cards.FromName("Frostwolf Grunt"),
+            Cards.FromName("Stormpike Commando"),
+            Cards.FromName("Elven Archer"),
+            Cards.FromName("Stormwind Champion"),
+            Cards.FromName("Frostwolf Warlord"),
+            Cards.FromName("Voodoo Doctor"),
+            Cards.FromName("Raid Leader"),
+            Cards.FromName("Kobold Geomancer"),
+            Cards.FromName("Sen'jin Shieldmasta"),
+            Cards.FromName("Voodoo Doctor"),
+            Cards.FromName("Frostwolf Warlord")
+        };
+    }
+}
--- SimpleAi/Program.cs
+++ SimpleAi/Program.cs
@@ -1,19 +1,28 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using HearthDb.Enums;
 using HsProtoSim.Config;
 using HsProtoSim.Model;
 using HsProtoSim.Tasks;
+using HsProtoSim.Tasks.PlayerTasks;
 using log4net;
+using SimpleAi.Meta;
 using SimpleAi.Nodes;
+using SimpleAi.Score;
 
 namespace SimpleAi
 {
-    class Program
+    internal class Program
     {
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -31,19 +40,31 @@ namespace SimpleAi
 
         public static void OptionNodeTest()
         {
-            var game = new Game(MageMirrorConfig(false));
+            var game = new Game(
+                new GameConfig()
+                {
+                    StartPlayer = 1,
+                    Player1Name = "FitzVonGerald",
+                    Player1HeroClass = CardClass.WARRIOR,
+                    DeckPlayer1 = Decks.WarriorPirate,
+                    Player2Name = "RehHausZuckFuchs",
+                    Player2HeroClass = CardClass.MAGE,
+                    DeckPlayer2 = Decks.SimpleMage,
+                    FillDecks = false,
+                    Shuffle = true
+                });
             game.StartGame();
-            game.Player1.BaseMana = 5;
             while (game.State != State.COMPLETE)
             {
-                Log.Info($"Player1: {game.Player1.PlayState} / Player2: {game.Player2.PlayState} " +
-                         $"----- TURN {game.Turn}");
+                Log.Info("");
+                Log.Info($"Player1: {game.Player1.PlayState} / Player2: {game.Player2.PlayState} - " +
+                         $"ROUND {(game.Turn + 1) / 2} - {game.CurrentPlayer.Name}");
                 Log.Info($"Hero[P1]: {game.Player1.Hero.Health} / Hero[P2]: {game.Player2.Hero.Health}");
-
+                Log.Info("");
                 while (game.CurrentPlayer == game.Player1)
                 {
                     Log.Info($"* Calculating solutions *** Player 1 ***");
-                    var solutions = OptionNode.GetSolutions(game, game.Player1.Id, 10, 1000);
+                    var solutions = OptionNode.GetSolutions(game, game.Player1.Id, 10, 500);
                     var solution = new List<PlayerTask>();
                     solutions.OrderByDescending(p => p.Score).First().PlayerTasks(ref solution);
                     Log.Info($"- Player 1 - <{game.CurrentPlayer.Name}> ---------------------------");
@@ -58,93 +79,24 @@ namespace SimpleAi
                 Log.Info($"- Player 2 - <{game.CurrentPlayer.Name}> ---------------------------");
                 while (game.CurrentPlayer == game.Player2)
                 {
-                    var options = game.Options(game.CurrentPlayer);
-                    var option = options[Rnd.Next(options.Count)];
-                    Log.Info($"[{option.FullPrint()}]");
-                    game.Process(option);
+                    //var options = game.Options(game.CurrentPlayer);
+                    //var option = options[Rnd.Next(options.Count)];
+                    //Log.Info($"[{option.FullPrint()}]");
+                    //game.Process(option);
+                    Log.Info($"* Calculating solutions *** Player 1 ***");
+                    var solutions = OptionNode.GetSolutions(game, game.Player2.Id, 10, 500);
+                    var solution = new List<PlayerTask>();
+                    solutions.OrderByDescending(p => p.Score).First().PlayerTasks(ref solution);
+                    Log.Info($"- Player 1 - <{game.CurrentPlayer.Name}> ---------------------------");
+                    solution.ForEach(p =>
+                    {
+                        Log.Info(p.FullPrint());
+                        game.Process(p);
+                    });
                 }
             }
             Log.Info($"Game: {game.State}, Player1: {game.Player1.PlayState} / Player2: {game.Player2.PlayState}");
         }
 
-        public static GameConfig MageMirrorConfig(bool shuffle)
-        {
-            return new GameConfig
-            {
-                StartPlayer = 1,
-                Player1Name = "FitzVonGerald",
-                Player1HeroClass = CardClass.MAGE,
-                DeckPlayer1 = new List<Card>()
-                {
-                    Cards.FromName("Mirror Image"),
-                    Cards.FromName("Sorcerer's Apprentice"),
-                    Cards.FromName("Ice Lance"),
-                    Cards.FromName("Frostbolt"),
-                    Cards.FromName("Ironforge Rifleman"),
-                    Cards.FromName("Mirror Image"),
-                    Cards.FromName("Ironforge Rifleman"),
-                    Cards.FromName("Gnomish Inventor"),
-                    Cards.FromName("Fireball"),
-                    Cards.FromName("Fireball"),
-                    Cards.FromName("Gnomish Inventor"),
-                    Cards.FromName("Ice Lance"),
-                    Cards.FromName("Sorcerer's Apprentice"),
-                    Cards.FromName("Frostwolf Grunt"),
-                    Cards.FromName("Raid Leader"),
-                    Cards.FromName("Kobold Geomancer"),
-                    Cards.FromName("Stormpike Commando"),
-                    Cards.FromName("Sen'jin Shieldmasta"),
-                    Cards.FromName("Elven Archer"),
-                    Cards.FromName("Frostwolf Grunt"),
-                    Cards.FromName("Stormpike Commando"),
-                    Cards.FromName("Elven Archer"),
-                    Cards.FromName("Stormwind Champion"),
-                    Cards.FromName("Frostwolf Warlord"),
-                    Cards.FromName("Voodoo Doctor"),
-                    Cards.FromName("Raid Leader"),
-                    Cards.FromName("Kobold Geomancer"),
-                    Cards.FromName("Sen'jin Shieldmasta"),
-                    Cards.FromName("Voodoo Doctor"),
-                    Cards.FromName("Frostwolf Warlord")
-                },
-                Player2Name = "RehHausZuckFuchs",
-                Player2HeroClass = CardClass.MAGE,
-                DeckPlayer2 = new List<Card>()
-                {
-                    Cards.FromName("Mirror Image"),
-                    Cards.FromName("Sorcerer's Apprentice"),
-                    Cards.FromName("Ice Lance"),
-                    Cards.FromName("Frostbolt"),
-                    Cards.FromName("Ironforge Rifleman"),
-                    Cards.FromName("Mirror Image"),
-                    Cards.FromName("Ironforge Rifleman"),
-                    Cards.FromName("Gnomish Inventor"),
-                    Cards.FromName("Fireball"),
-                    Cards.FromName("Fireball"),
-                    Cards.FromName("Gnomish Inventor"),
-                    Cards.FromName("Ice Lance"),
-                    Cards.FromName("Sorcerer's Apprentice"),
-                    Cards.FromName("Frostwolf Grunt"),
-                    Cards.FromName("Raid Leader"),
-                    Cards.FromName("Kobold Geomancer"),
-                    Cards.FromName("Stormpike Commando"),
-                    Cards.FromName("Sen'jin Shieldmasta"),
-                    Cards.FromName("Elven Archer"),
-                    Cards.FromName("Frostwolf Grunt"),
-                    Cards.FromName("Stormpike Commando"),
-                    Cards.FromName("Elven Archer"),
-                    Cards.FromName("Stormwind Champion"),
-                    Cards.FromName("Frostwolf Warlord"),
-                    Cards.FromName("Voodoo Doctor"),
-                    Cards.FromName("Raid Leader"),
-                    Cards.FromName("Kobold Geomancer"),
-                    Cards.FromName("Sen'jin Shieldmasta"),
-                    Cards.FromName("Voodoo Doctor"),
-                    Cards.FromName("Frostwolf Warlord")
-                },
-                FillDecks = false,
-                Shuffle = shuffle,
-            };
-        }
     }
 }
